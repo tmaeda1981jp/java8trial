39ab894f15f32c55abf9cb1bb878560d31f37e04
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-03-22 18:04:58 +0900

    Add ComparatorSample.java

---
+++ src/main/java/jp/smilecode/java8trial/stream/ComparatorSample.java
@@ -0,0 +1,15 @@
+package jp.smilecode.java8trial.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ComparatorSample {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList(
+                "Java", "Groovy", "Scala", "Python", "Ruby", "Perl", "C"
+        );
+        System.out.println(list);
+        list.sort((String s1, String s2) -> s1.length() - s2.length());
+        System.out.println(list);
+    }
+}
