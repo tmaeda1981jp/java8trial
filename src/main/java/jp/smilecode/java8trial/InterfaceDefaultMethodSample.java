a3a0020bf6ed55ef73006aa5f829d71aea704df4
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-04-19 21:04:41 +0900

    Add memos

--- src/main/java/jp/smilecode/java8trial/InterfaceDefaultMethodSample.java
+++ src/main/java/jp/smilecode/java8trial/InterfaceDefaultMethodSample.java
@@ -10,21 +10,25 @@ public class InterfaceDefaultMethodSample {
         }.print("test");
         new Printable(){}.print("test");
         
-        // NG
+        // 関数型インターフェースじゃないのでこの書き方はNG
         // ((Printable)(str) -> System.out.printf("[[ %s ]]\n", str)).print("hoge");
         
-        // OK
+        // こちらは関数型インターフェースなのでこの書き方でOK
         ((Writable) (str) -> System.out.printf("[[ %s ]]\n", str)).write("hoge");
     }
 }
 
 
+// 抽象メソッドがひとつもないので関数型インターフェースじゃない
+// @FunctionalInterface
 interface Printable {
     default public void print(String str) {
         System.out.println(str);
     }
 }
 
+// 抽象メソッドがひとつだけなので関数型インターフェース
+@FunctionalInterface
 interface Writable {
     void write(String str);   
 }
