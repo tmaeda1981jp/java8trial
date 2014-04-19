fd966dd39311010798a55b9c8c239cd306ab3fe2
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-04-20 01:35:37 +0900

    Add FunctionalInterface annotation, and sample codes

--- src/main/java/jp/smilecode/java8trial/FunctionalInterfaceSample.java
+++ src/main/java/jp/smilecode/java8trial/FunctionalInterfaceSample.java
@@ -1,9 +1,20 @@
 package jp.smilecode.java8trial;
 
 public class FunctionalInterfaceSample {
-    interface Counter { int count(String s1, String s2); }
+
     public static void main(String[] args) {
         Counter counter = (s1, s2) -> s1.length() + s2.length();
         System.out.println(counter.count("abc", "de"));
+
+        int result = ((Counter) (s1, s2) -> s1.length() + s2.length())
+                .count("abc", "de");
+        System.out.println(result);
+
     }
+}
+
+@FunctionalInterface
+interface Counter {
+
+    int count(String s1, String s2);
 }
