e841d7853290298286b6b56662643fdfcc348dd4
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-04-24 09:02:14 +0900

    Add FunctionalInterface2 sample

---
+++ src/main/java/jp/smilecode/java8trial/FunctionalInterfaceSample2.java
@@ -0,0 +1,26 @@
+package jp.smilecode.java8trial;
+
+import java.util.Date;
+
+public class FunctionalInterfaceSample2 {
+
+    public static void main(String[] args) {
+        Function<Date> f1 = Date::new;
+        Date date = f1.apply();
+        System.out.println(date);
+        
+        Function2<String> f2 = (str) -> new String(str);
+        String str = f2.apply("test");
+        System.out.println(str);
+    }
+    
+    @FunctionalInterface
+    interface Function<T> {
+        T apply();
+    }
+    
+    @FunctionalInterface
+    interface Function2<T> {
+        T apply(String str);
+    }
+}
