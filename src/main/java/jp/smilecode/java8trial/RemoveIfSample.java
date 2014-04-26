e38d1c630565325b0baf35341b8822fabeed49cf
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-04-27 00:14:18 +0900

    Update

--- src/main/java/jp/smilecode/java8trial/RemoveIfSample.java
+++ src/main/java/jp/smilecode/java8trial/RemoveIfSample.java
@@ -8,12 +8,7 @@ public class RemoveIfSample {
 
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
-        System.out.println(list.getClass());
         list.removeIf(n -> (n % 3 == 0));
         System.out.println(list);
-        
-        List<Integer> list2 = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-        System.out.println(list2.getClass());
-        list2.removeIf(n -> (n % 3 == 0)); // throw UnsupportedOperationException
     }
 }
