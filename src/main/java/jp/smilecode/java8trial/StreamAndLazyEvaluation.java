ffd701ae72130a3e89990aa5714a5da74c69f703
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-04-28 21:59:32 +0900

    Fix StreamAndLazyEvaluation sample

--- src/main/java/jp/smilecode/java8trial/StreamAndLazyEvaluation.java
+++ src/main/java/jp/smilecode/java8trial/StreamAndLazyEvaluation.java
@@ -1,13 +1,14 @@
 package jp.smilecode.java8trial;
 
-import java.util.stream.IntStream;
-//import java.util.stream.Stream;
+import java.util.stream.Stream;
+
 
 public class StreamAndLazyEvaluation {
    
     public static void main(String[] args) {
-        //Stream<Integer> stream = Stream.iterate(1, i -> i++);
-        IntStream stream = IntStream.range(1, 100);
-        stream.limit(20).forEach(System.out::println);
+        Stream.iterate(1, x -> x++) // 無限リスト
+                .limit(100)
+                .forEach(System.out::println);
+                
     }
 }
