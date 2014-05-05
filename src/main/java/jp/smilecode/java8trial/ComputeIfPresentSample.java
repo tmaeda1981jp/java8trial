98507cd3a61377fbae0165fd12b7e6cb27703b4c
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-05-06 00:17:48 +0900

    Add ComputeIfPresentSample

---
+++ src/main/java/jp/smilecode/java8trial/ComputeIfPresentSample.java
@@ -0,0 +1,21 @@
+package jp.smilecode.java8trial;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ComputeIfPresentSample {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap();
+        map.put("a", "abc");
+        map.put("b", "bcd");
+        map.put("c", "cde");
+        
+        Arrays.asList("a", "c", "f").stream()
+                .forEach(key -> {
+                    map.computeIfPresent(key, (k, v) -> v.toUpperCase());
+                });
+        System.out.println(map);
+    }
+}
