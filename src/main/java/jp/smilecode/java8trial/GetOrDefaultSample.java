55f9c948e219e9e9a5de8fba4d45cfe4e6c0a499
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-05-06 00:02:40 +0900

    Add Map#getOrDefault sample

---
+++ src/main/java/jp/smilecode/java8trial/GetOrDefaultSample.java
@@ -0,0 +1,23 @@
+package jp.smilecode.java8trial;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetOrDefaultSample {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap();
+        map.put("a", "abc");
+        map.put("b", "bcd");
+        map.put("c", "cde");
+        
+        System.out.println(map.get("a"));
+        System.out.println(map.get("b"));
+        System.out.println(map.get("c"));
+        System.out.println(map.get("d"));
+
+        System.out.println(map.getOrDefault("a", "DEFAULT"));
+        System.out.println(map.getOrDefault("b", "DEFAULT"));
+        System.out.println(map.getOrDefault("c", "DEFAULT"));
+        System.out.println(map.getOrDefault("d", "DEFAULT"));
+    }
+}
